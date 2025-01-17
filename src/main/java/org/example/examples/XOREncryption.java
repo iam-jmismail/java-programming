1a2c0ab664db125075091b9040593e438e1796a0
Mohamed Ismail <jmismail.dev@gmail.com>
2025-01-17 14:48:21 +0530

    feat: xor encryption with base64 encoding

--- src/main/java/org/example/examples/XOREncryption.java
+++ src/main/java/org/example/examples/XOREncryption.java
@@ -1,15 +1,44 @@
 package org.example.examples;
 
+import java.util.Base64;
+
 public class XOREncryption {
     public static void main(String[] args) {
 
         String message = "Hello World!";
-        int key = 0xAA; // 170 in decimal
+//        int key = 0xAA; // 170 in decimal
+        int key = 1024; // 170 in decimal
+
+        // Encrypting
         StringBuilder encryptedString = new StringBuilder();
 
         for (int i =0 ; i< message.length() ; i++) {
             encryptedString.append((char) (message.charAt(i) ^ key));
         }
-        System.out.println(encryptedString);
+
+        // String to Bytes
+        byte[] encryptedBytes = encryptedString.toString().getBytes();
+
+        // Encode Bytes to Base 64
+        String base64Encoded = Base64.getEncoder().encodeToString(encryptedBytes);
+        System.out.println(base64Encoded);
+
+        // Decrypting
+
+        // Decode base64 back to Bytes array
+        byte[] decodedBytes = Base64.getDecoder().decode(base64Encoded);
+
+        // Convert Bytes back to String
+        String decodedEncryptedString = new String(decodedBytes);
+
+        // Decrypt
+        StringBuilder decryptedString = new StringBuilder();
+        for (int i = 0; i < decodedEncryptedString.length(); i++) {
+            decryptedString.append((char) (decodedEncryptedString.charAt(i) ^ key));
+        }
+
+        System.out.println("Decrypted Message: " + decryptedString);
+
+
     }
 }
